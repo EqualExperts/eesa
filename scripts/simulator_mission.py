40705ab7608494d199196b544468203aef89855f
Tim Squires <tim@crystalmark.co.uk>
2017-11-25 13:59:56 +0000

    Removed relative altitude listeners.  Changed simulator script to inject current altitude + 100m.

--- scripts/simulator_mission.py
+++ scripts/simulator_mission.py
@@ -1,7 +1,17 @@
 import mission
+from dronekit import connect as drone_connect, mavutil, VehicleMode
 
 #Connect to remote SITL instance
 connection_string = "udp:127.0.0.1:5769"
 
-mission.start_flight(connection_string)
+vehicle = drone_connect(connection_string, wait_ready=True)
+while not vehicle.location.global_frame.alt and not self.stop():
+    self.log( "No GPS signal yet" )
+    time.sleep(1)
+current_alt=vehicle.location.global_frame.alt
+release_alt=current_alt+100
+print("Current altitude = %s so release altitude = %s" % (current_alt, release_alt))
+vehicle.close()
+
+mission.start_flight(connection_string, release_alt)
 
