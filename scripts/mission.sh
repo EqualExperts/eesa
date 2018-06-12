178bd4b0e019833cde990e90ea4249ab011e3ddd
Tim Squires <tim@crystalmark.co.uk>
2018-06-12 19:48:00 +0100

    gzip logs on start

--- scripts/mission.sh
+++ scripts/mission.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-sleep 3m
+# sleep 3m
 cd /home/apsync
-python mission.py
+until [ -f /home/apsync/stopmission ]
+do
+  gzip -c mission.log  > mission.log."$(date +%s)".gz;
+  python mission.py 2>&1 > mission.log
+  sleep 5
+done
 # TODO loop unless stopmission found incase wait timeout?
