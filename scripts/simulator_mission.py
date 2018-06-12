1676a754a7ede2bbb4fd0ea4f637dc0452d95cb0
Tim Squires <tim@crystalmark.co.uk>
2018-06-12 19:51:14 +0100

    Fix bug with sim script and print position on start

--- scripts/simulator_mission.py
+++ scripts/simulator_mission.py
@@ -5,10 +5,12 @@ args = ['--model', 'plane',]
 
 sitl = SITL() # load a binary path (optional)
 sitl.download("plane", "3.3.0", verbose=False)
-sitl.launch(args, verbose=False, await_ready=Falses, restart=False)
+sitl.launch(args, verbose=False, await_ready=True, restart=False)
 sitl.block_until_ready(verbose=False)
 
 mission.start_flight(sitl.connection_string())
+
+print(sitl.position)
 
 code = sitl.complete(verbose=False)
 sitl.poll()
