0b2250937813206c8e8a7e75bdc3c8044f3ecb6f
Tim Squires <tim@crystalmark.co.uk>
2018-10-15 22:09:08 +0100

    Lots of changes.  Now tested at multiple altitudes above Devils Beef Tub

--- scripts/flight.py
+++ scripts/flight.py
@@ -8,8 +8,6 @@ from pymavlink import mavutil
 
 class Flight(object):
 
-	home = { 'lat': 123.456, 'lng': 567.890, 'alt': 90 }
-
 	min_release_alt=2000
 	max_release_alt=30000
 	min_throttle=5
@@ -22,14 +20,14 @@ class Flight(object):
 	maximum_calculated_throttle=50
 	minimum_calculated_min_pitch=4500
 	maximum_calculated_min_pitch=9000
-	minimum_calculated_max_pitch=0
-	maximum_calculated_max_pitch=4
+	minimum_calculated_max_pitch=500
+	maximum_calculated_max_pitch=3000
 
-	altitude_step=100
+	altitude_step=250
 	
-	def __init__(self, vehicle):
+	def __init__(self, vehicle, home):
 		self.vehicle = vehicle
-		# self.set_home()
+		self.home = home
 		self.vehicle.parameters['TKOFF_THR_MINACC']=2
 		self.vehicle.parameters['LIM_PITCH_MAX']=700 #(i.e. can't climb)
 		self.vehicle.parameters['LIM_PITCH_MIN']=-9000 #(straight down)
@@ -45,37 +43,36 @@ class Flight(object):
 		self.alt = alt
 	
 	def takeoff(self):
-		self.arm()
 
-		self.alt=self.vehicle.location.global_relative_frame.alt
+		self.set_home()
+
 		if self.alt > self.min_release_alt:
 			time.sleep(5) # wait for a little speed to build up
 		self.load_mission()
 	
+		self.arm("FBWA")
 		self.change_mode("AUTO")
 	
 		self.log("Delaying to allow takeoff to happen")
 		time.sleep(5)
-	
-		## ????  Use global_rel_frame or inject alt from listener ????
-		self.alt=self.vehicle.location.global_relative_frame.alt
+
 		while self.alt > self.min_release_alt:
 			if self.alt < 5000:
 				## Only use airspeed sensor when below 5km
 				self.vehicle.parameters['ARSPD_USE']=1
 
 			## Calculate graduated target speed, stall speed, throttle and pitch
-			mission_percentage=((alt-min_release_alt)/(max_release_alt-min_release_alt))*100
-			target_speed=int((maximum_calculated_target_speed-minimum_calculated_target_speed)*(mission_percentage/100))+minimum_calculated_target_speed
+			mission_percentage=((self.alt-self.min_release_alt)/(self.max_release_alt-self.min_release_alt))*100
+			target_speed=int((self.maximum_calculated_target_speed-self.minimum_calculated_target_speed)*(mission_percentage/100))+self.minimum_calculated_target_speed
 			max_speed=target_speed+10
-			min_speed=int((maximum_calculated_min_speed-minimum_calculated_min_speed)*(mission_percentage/100))+minimum_calculated_min_speed
-			min_pitch=int((maximum_calculated_min_pitch-minimum_calculated_min_pitch)*(mission_percentage/100))+minimum_calculated_min_pitch
-			throttle=maximum_calculated_throttle-int((maximum_calculated_throttle-minimum_calculated_throttle)*(mission_percentage/100))
-			max_pitch=maximum_calculated_max_pitch-int((maximum_calculated_max_pitch-minimum_calculated_max_pitch)*(mission_percentage/100))
+			min_speed=int((self.maximum_calculated_min_speed-self.minimum_calculated_min_speed)*(mission_percentage/100))+self.minimum_calculated_min_speed
+			min_pitch=int((self.maximum_calculated_min_pitch-self.minimum_calculated_min_pitch)*(mission_percentage/100))+self.minimum_calculated_min_pitch
+			throttle=self.maximum_calculated_throttle-int((self.maximum_calculated_throttle-self.minimum_calculated_throttle)*(mission_percentage/100))
+			max_pitch=self.maximum_calculated_max_pitch-int((self.maximum_calculated_max_pitch-self.minimum_calculated_max_pitch)*(mission_percentage/100))
 			## TODO Use logger class ????
-			self.log ("Alt "+str(alt)+"m, "+str(int(mission_percentage))+"%, max_pitch "+str(max_pitch)+"m/s, min_pitch "+str(min_pitch))
-			self.log ("Alt "+str(alt)+"m, "+str(int(mission_percentage))+"%, max_speed "+str(max_speed)+"m/s, target "+str(target_speed)+"m/s, min_speed "+str(min_speed)+"m/s")
-			self.log ("Alt "+str(alt)+"m, "+str(int(mission_percentage))+"%, throttle "+str(throttle)+"%")
+			self.log ("Alt "+str(self.alt)+"m, "+str(int(mission_percentage))+"%, max_pitch "+str(max_pitch)+", min_pitch "+str(min_pitch))
+			self.log ("Alt "+str(self.alt)+"m, "+str(int(mission_percentage))+"%, max_speed "+str(max_speed)+"m/s, target "+str(target_speed)+"m/s, min_speed "+str(min_speed)+"m/s")
+			self.log ("Alt "+str(self.alt)+"m, "+str(int(mission_percentage))+"%, throttle "+str(throttle)+"%")
 			self.log ("----")
 
 			self.vehicle.parameters['LIM_PITCH_MAX']=max_pitch
@@ -85,11 +82,11 @@ class Flight(object):
 			self.vehicle.parameters['ARSPD_FBW_MAX']=max_speed
 			self.vehicle.parameters['ARSPD_FBW_MIN']=min_speed
 			self.vehicle.parameters['TRIM_ARSPD_CM']=target_speed*100
+			self.vehicle.flush()
 			self.goto_altitude(self.alt - self.altitude_step)
 			## Allow the flight controller to do it's thing for 60 seconds
 			time.sleep(60)
-			alt=self.vehicle.location.global_relative_frame.alt
-	
+
 		## Last approach to primary landing site
 		self.vehicle.parameters['ARSPD_USE']=1
 		self.vehicle.parameters['ARSPD_FBW_MAX']=25
@@ -97,15 +94,17 @@ class Flight(object):
 		self.vehicle.parameters['TRIM_ARSPD_CM']=12
 		self.vehicle.parameters['STAB_PITCH_DOWN']=2
 		
-		self.vehicle.parameters['LIM_PITCH_MAX']=4 # ???? Whats the default ???
+		self.vehicle.parameters['LIM_PITCH_MAX']=4500 # ???? Whats the default ???
 		self.vehicle.parameters['LIM_PITCH_MIN']=-4500 # ???? Whats the default ???
 		self.vehicle.parameters['TRIM_THROTTLE']=33
 		self.vehicle.parameters['THR_MAX']=75
 		
-		self.change_mode("RTL")
+		self.goto_altitude(self.home['alt'])
 
 		## Keep the script alive until landing
-		while self.alt > 0:
+		self.log( "alt = "+str(self.alt))
+		while True:
+			self.log( "Waiting to land, alt = "+str(self.alt))
 			time.sleep(300)
 
 		self.log ("Landed")
@@ -117,43 +116,57 @@ class Flight(object):
 			self.vehicle.mode = mode
 			time.sleep(1)
 	
+	def load_mission(self):
+		lat=self.vehicle.location.global_frame.lat
+		lng=self.vehicle.location.global_frame.lon
+		take_off_alt=self.alt*1.1
+		cmds = self.vehicle.commands
+		cmds.clear()
+		self.log( "Loading mission with takeoff from %s to %s meters" % (self.alt,take_off_alt) )
+		# Repeat first command because of bug in SITL
+		cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 45, 0, 0, 0, 0, 0, 100))
+		cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 45, 0, 0, 0, 0, 0, 100))
+		cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL, mavutil.mavlink.MAV_CMD_NAV_LOITER_UNLIM, 0, 0, 100, 0, 0, 0, lat, lng, take_off_alt))
+		self.vehicle.flush()
+
+	def goto_altitude(self, target_altitude):
+		self.log( "Heading to target altitude %s meters" % (target_altitude) )
+		self.change_mode("GUIDED")
+		point = LocationGlobal(self.home['lat'], self.home['lng'], target_altitude)
+		self.vehicle.simple_goto(point)
+		## self.change_mode("GUIDED")
+		## cmds = self.vehicle.commands
+		## cmds.clear()
+		## cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL, mavutil.mavlink.MAV_CMD_NAV_LOITER_UNLIM, 0, 0, 100, 0, 0, 0, self.home['lat'], self.home['lng'], self.home['alt']))
+		## self.vehicle.flush()
+
+	def arm(self, mode):
+		self.change_mode(mode)
+		self.vehicle.armed=True
+		self.vehicle.flush
+		while not self.vehicle.armed:
+			self.log(" Waiting for arming...")
+			time.sleep(1)
+			self.vehicle.armed=True
+
 	def set_home(self):
 		while not self.vehicle.home_location:
 			cmds = self.vehicle.commands
 			cmds.download()
 			cmds.wait_ready()
 			if not self.vehicle.home_location:
-				self.log(" Waiting for home location ...")
-				time.sleep(1)
+				self.log("Waiting for home location ...")
+				tims.sleep(1)
 		self.log("Old home location: %s" % self.vehicle.home_location)
-
-		self.vehicle.home_location=LocationGlobal(self.home['lat'], self.home['lng'], self.home['alt'])
-
-	def load_mission(self):
-		self.alt=self.vehicle.location.global_relative_frame.alt
-		lat=self.vehicle.location.global_relative_frame.lat
-		lng=self.vehicle.location.global_relative_frame.lon
-		take_off_alt=100
-		if self.alt > 100:
-			take_off_alt=0
-		cmds = self.vehicle.commands
-		cmds.clear()
-		cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 0, take_off_alt))
-		cmds.add(Command( 0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL, mavutil.mavlink.MAV_CMD_NAV_LOITER_UNLIM, 0, 0, 100, 0, 0, 0, lat, lng, self.alt))
-		self.vehicle.flush()
-
-	def goto_altitude(self, target_altitude):
-		self.change_mode("GUIDED")
-		point = LocationGlobal(self.home.latitude, self.home.longitude, target_altitude)
-		vehicle.simple_goto(point)
-
-	def arm(self):
-		self.change_mode("FBWA")
-		self.vehicle.armed=True
-                self.vehicle.flush()
-	    	while not self.vehicle.armed:
-	        	self.log(" Waiting for arming...")
-	        	time.sleep(1)
+		self.vehicle.home_location=LocationGlobal(self.home['lat'], self.home['lng'],self.home['alt'])
+		while not self.vehicle.home_location:
+			cmds = self.vehicle.commands
+			cmds.download()
+			cmds.wait_ready()
+			if not self.vehicle.home_location:
+				self.log("Waiting for home location ...")
+				tims.sleep(1)
+		self.log("NEW home location: %s" % self.vehicle.home_location)
 
 	def log(self, message):
 		print ( message )
--- scripts/mission.py
+++ scripts/mission.py
@@ -18,197 +18,177 @@ from dronekit import connect as drone_connect, mavutil, VehicleMode
 
 class Drone(object):
 
-    def __init__(self, connection_string, release_altitude=50):
-        self.connection_string = connection_string
-        self.closed_pwm = 1890
-        self.open_pwm = 1280
-        self.twitch=10
-        self.release_servo_number = 9 # aux 1
-        self.test_servo_numbers = []
-        # self.test_servo_numbers = [11,12,13]
-        self.release_altitude = release_altitude
-        self.current_test_servo_pwm = self.closed_pwm
-        self.released = False
-        self.flight_mission_started = False
+	# Iceland
+	#home = { 'lat': 123.456, 'lng': 567.890, 'alt': 90 }
+	# Devils Beef Tub
+	home = { 'lat': 55.404, 'lng': -3.486, 'alt': 440 }
+	# Penicuik
+	#home = { 'lat': 55.807, 'lng': -3.248, 'alt': 302 }
 
-	self.home_alt = 262
+	def __init__(self, connection_string, release_altitude=50):
+		self.connection_string = connection_string
+		self.closed_pwm = 1890
+		self.open_pwm = 1280
+		self.twitch=10
+		self.release_servo_number = 9 # aux 1
+		self.test_servo_numbers = []
+		# self.test_servo_numbers = [11,12,13]
+		self.release_altitude = release_altitude
+		self.current_test_servo_pwm = self.closed_pwm
+		self.released = False
+		self.flight_mission_started = False
 
-        logging.basicConfig(format='%(asctime)-15s %(clientip)s %(user)-8s %(message)s')
-        self.logger = logging.getLogger('mission_log')
-        self.logger.addHandler(RotatingFileHandler('mission.log', maxBytes=10000000, backupCount=0))
+		self.flight = None
 
-    def set_servo(self, servo_number, pwm_value):
-        pwm_value_int = int(pwm_value)
-        msg = self.connection.message_factory.command_long_encode(
-            0, 0,  # target system, target component
-            mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
-            0,
-            servo_number,
-            pwm_value_int,
-            0, 0, 0, 0, 0
-        )
-        self.connection.send_mavlink(msg)
+		logging.basicConfig(format='%(asctime)-15s %(clientip)s %(user)-8s %(message)s')
+		self.logger = logging.getLogger('mission_log')
+		self.logger.addHandler(RotatingFileHandler('mission.log', maxBytes=10000000, backupCount=0))
 
-    def log(self, message):
-        logmessage = "%s %s" % (time.strftime("%Y %m %d %H:%M:%S", time.gmtime()), message )
-        self.logger.info(logmessage)
-        print(logmessage)
+	def set_servo(self, servo_number, pwm_value):
+		pwm_value_int = int(pwm_value)
+		msg = self.connection.message_factory.command_long_encode(
+			0, 0,  # target system, target component
+			mavutil.mavlink.MAV_CMD_DO_SET_SERVO,
+			0,
+			servo_number,
+			pwm_value_int,
+			0, 0, 0, 0, 0
+		)
+		self.connection.send_mavlink(msg)
 
-    # Moves servo 9 (aux 1) to release payload
-    def release_payload(self):
-        self.log("!!!!!RELEASING PAYLOAD!!!!!")
-        start_new_thread(self.set_servo, (self.release_servo_number, self.open_pwm,))
-        self.released = True
-        start_new_thread(self.start_flight_mission, ())
+	def log(self, message):
+		logmessage = "%s %s" % (time.strftime("%Y %m %d %H:%M:%S", time.gmtime()), message )
+		self.logger.info(logmessage)
+		print(logmessage)
 
-    # Moves servo 9 (aux 1) to closed to hold payload
-    def lock_payload(self):
-        self.log("!!!!!LOCKING PAYLOAD!!!!!")
-        start_new_thread(self.set_servo, (self.release_servo_number, self.closed_pwm,))
-        self.released = False
+	# Moves servo 9 (aux 1) to release payload
+	def release_payload(self):
+		self.log("!!!!!RELEASING PAYLOAD!!!!!")
+		start_new_thread(self.set_servo, (self.release_servo_number, self.open_pwm,))
+		self.released = True
+		if not self.flight_mission_started:
+			self.flight_mission_started = True
+			start_new_thread(self.start_flight_mission, ())
+
+	# Moves servo 9 (aux 1) to closed to hold payload
+	def lock_payload(self):
+		self.log("!!!!!LOCKING PAYLOAD!!!!!")
+		start_new_thread(self.set_servo, (self.release_servo_number, self.closed_pwm,))
+		self.released = False
 
 
-    # Main mission - should be executed after release
-    def start_flight_mission(self):
-	flight = Flight(self.connection)
-	flight.takeoff()
+	# Main mission - should be executed after release
+	def start_flight_mission(self):
+		self.flight = Flight(self.connection, self.home)
+		self.flight.takeoff()
 
-    def set_home(self):
-        msg = self.connection.message_factory.command_long_encode(
-            0, 0,
-            mavutil.mavlink.MAV_CMD_DO_SET_HOME,
-            0,
-            2, 
-            0, 0, 0,
-            55.806312,
-            -3.247739,
-            262
-            # self.home_alt ???
-        )
-        self.connection.send_mavlink(msg)
-        self.connection.flush()
+	# Twitches servo 9 (aux 1) a random amount to keep it warm on the way up
+	def twitch_release_servo(self):
+		if not self.released:
+			min_twitch = self.closed_pwm-self.twitch
+			max_twitch = self.closed_pwm+self.twitch
+			twitch_pwm = random.randint(min_twitch, max_twitch)
+			start_new_thread(self.set_servo, (self.release_servo_number, twitch_pwm))
 
-    # Twitches servo 9 (aux 1) a random amount to keep it warm on the way up
-    def twitch_release_servo(self):
-        if not self.released:
-            min_twitch = self.closed_pwm-self.twitch
-            max_twitch = self.closed_pwm+self.twitch
-            twitch_pwm = random.randint(min_twitch, max_twitch)
-            start_new_thread(self.set_servo, (self.release_servo_number, twitch_pwm))
+	# Moves servo 9 (aux 1) to closed to hold payload
+	def move_test_servos(self):
+		if not self.released:
+			self.log("moving test servos")
+			for servo_number in self.test_servo_numbers:
+				start_new_thread(self.set_servo, (servo_number, self.current_test_servo_pwm,))
+			if self.current_test_servo_pwm != self.closed_pwm:
+				self.current_test_servo_pwm = self.closed_pwm
+			else:
+				self.current_test_servo_pwm = self.open_pwm
 
-    # Moves servo 9 (aux 1) to closed to hold payload
-    def move_test_servos(self):
-        if not self.released:
-            self.log("moving test servos")
-            for servo_number in self.test_servo_numbers:
-                start_new_thread(self.set_servo, (servo_number, self.current_test_servo_pwm,))
-            if self.current_test_servo_pwm != self.closed_pwm:
-                self.current_test_servo_pwm = self.closed_pwm
-            else:
-                self.current_test_servo_pwm = self.open_pwm
+	def connect(self):
+		self.connection = drone_connect(self.connection_string, wait_ready=True)
+		return self.connection
 
-    def connect(self):
-        self.connection = drone_connect(self.connection_string, wait_ready=True)
-        return self.connection
+	def report(self):
+		self.log(" GPS: %s" % self.connection.gps_0)
+		self.log(" Battery: %s" % self.connection.battery)
+		self.log(" Last Heartbeat: %s" % self.connection.last_heartbeat)
+		self.log(" Is Armable?: %s" % self.connection.is_armable)
+		self.log(" System status: %s" % self.connection.system_status.state)
+		self.log(" Mode: %s" % self.connection.mode.name)
 
-    def report(self):
-        self.log(" GPS: %s" % self.connection.gps_0)
-        self.log(" Battery: %s" % self.connection.battery)
-        self.log(" Last Heartbeat: %s" % self.connection.last_heartbeat)
-        self.log(" Is Armable?: %s" % self.connection.is_armable)
-        self.log(" System status: %s" % self.connection.system_status.state)
-        self.log(" Mode: %s" % self.connection.mode.name)
+	def stop(self):
+		return os.path.isfile("/home/apsync/stopmission")
 
-    def stop(self):
-        return os.path.isfile("/home/apsync/stopmission")
+	def release_now(self):
+		return os.path.isfile("/home/apsync/releasenow")
 
-    def release_now(self):
-        return os.path.isfile("/home/apsync/releasenow")
+	def autopilot(self):
 
-    def autopilot(self):
+		# TODO use GPS Fix = 3D before allowing continue?
+		# TODO check if safety switch activated?
+		while not self.connection.is_armable and not self.stop():
+			self.log( "Initialising...." )
+			time.sleep(5)
 
-        # TODO use GPS Fix = 3D before allowing continue?
-        # TODO check if safety switch activated?
-        while not self.connection.is_armable and not self.stop():
-            self.log( "Initialising...." )
-            time.sleep(5)
+		self.log("Desired release altitude = %s" % self.release_altitude )
 
-	self.log("Desired release altitude = %s" % self.release_altitude )
+		# Make sure the payload release is in the closed position
+		self.lock_payload()
 
-        # Make sure the payload release is in the closed position
-        self.lock_payload()
+		while not self.connection.armed and not self.stop():
+			self.log( "Waiting for aircraft to be armed...." )
+			time.sleep(5)
 
-	while not self.connection.armed and not self.stop():
-	    self.log( "Waiting for aircraft to be armed...." )
-            time.sleep(5)
+		while not self.stop():
+			if self.release_now():
+				self.release_payload()
+				time.sleep(10)
+				break;
+			self.move_test_servos()
+			time.sleep(3)
 
-        while not self.stop():
-            if self.release_now():
-                self.release_payload()
-                time.sleep(10)
-                break;
-            self.move_test_servos()
-            time.sleep(3)
-
-        logging.shutdown()
-        self.connection.close()
+		logging.shutdown()
+		self.connection.close()
 
 def start_flight(connection_string):
-    drone = Drone(connection_string)
-    drone.log("Connecting to plane on %s" % (drone.connection_string,))
-    connection = drone.connect()
-    nowish = 1501926112 # Aug 5th 2017
+	drone = Drone(connection_string)
+	drone.log("Connecting to plane on %s" % (drone.connection_string,))
+	connection = drone.connect()
+	nowish = 1501926112 # Aug 5th 2017
 
-    @connection.on_message('SYSTEM_TIME')
-    def listenerTime(vehicle, name, message):
-        if connection.gps_0.fix_type == 3:
-            gpstime = int(message.time_unix_usec)/1000000
-            systime = int(time.time())
+	@connection.on_message('SYSTEM_TIME')
+	def listenerTime(vehicle, name, message):
+		if connection.gps_0.fix_type == 3:
+			gpstime = int(message.time_unix_usec)/1000000
+			systime = int(time.time())
 
-            if gpstime > nowish:
-                difftime = math.fabs(gpstime - systime)
-                if difftime > 60:
-                    drone.log( "gps time %s" % gpstime )
-                    drone.log( "sys time %s" % systime )
-                    drone.log( "diff %s" % difftime )
-                    drone.log("Setting system time to match GPS")
-                    if sys.platform in ['linux', 'linux2', 'darwin']:
-                        os.system("sudo date +%s -s @%s" % ('%s', gpstime))
-            else:
-                drone.log( "The GPS returned time since boot instead of time since epoch so can't use it for system time" )
+			if gpstime > nowish:
+				difftime = math.fabs(gpstime - systime)
+				if difftime > 60:
+					drone.log( "gps time %s" % gpstime )
+					drone.log( "sys time %s" % systime )
+					drone.log( "diff %s" % difftime )
+					drone.log("Setting system time to match GPS")
+					if sys.platform in ['linux', 'linux2', 'darwin']:
+						os.system("sudo date +%s -s @%s" % ('%s', gpstime))
+			else:
+				drone.log( "The GPS returned time since boot instead of time since epoch so can't use it for system time" )
 
-##    @connection.on_message('GPS_RAW_INT')
-    def listener_raw_gps(vehicle, name, message):
-        alt = message.alt/1000
-        drone.log( "RAW Alt %s" % alt )
-        if alt >= drone.release_altitude and not drone.released:
-            drone.log ( "Releasing payload at %s metres AMSL RAW" % alt)
-            drone.release_payload()
+	@connection.on_attribute('location.global_frame')
+	def listener_position(vehicle, name, message):
+		alt = message.alt
+		rel_alt = alt - drone.home['alt']
+		ts = int(time.time()*10)
+		if ts % 25 == 0:
+			drone.log( "Rel Alt %s" % rel_alt )
+		if drone.flight:
+			drone.flight.alt = rel_alt
+		if rel_alt >= drone.release_altitude and not drone.released:
+			drone.log ( "Releasing payload at %s metres relative to home" % rel_alt)
+			drone.release_payload()
+		else:
+			drone.twitch_release_servo()
 
-    @connection.on_attribute('GLOBAL_POSITION_INT')
-    def listener_gps(vehicle, name, message):
-        alt = message.alt/1000
-        drone.log( "Est Alt %s" % alt )
-        if alt >= drone.release_altitude and not drone.released:
-            drone.log ( "%s Releasing payload at %s metres AMSL" % (time.time(),alt))
-            drone.release_payload()
-
-    @connection.on_attribute('location.global_frame')
-    def listener_position(vehicle, name, message):
-        alt = message.alt
-	rel_alt = alt - drone.home_alt
-	ts = int(time.time()*10)
-	if ts % 10 == 0:
-            drone.log( "Rel Alt %s" % rel_alt )
-        if rel_alt >= drone.release_altitude and not drone.released:
-            drone.log ( "Releasing payload at %s metres relative to home" % rel_alt)
-            drone.release_payload()
-        else:
-            drone.twitch_release_servo()
-
-    drone.report()
-    drone.autopilot()
+	drone.report()
+	drone.autopilot()
 
 if __name__ == '__main__':
-    start_flight('0.0.0.0:9000')
+	start_flight('0.0.0.0:9000')
 
